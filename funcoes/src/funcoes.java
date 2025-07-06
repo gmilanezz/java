dbfd94cce8094556d697e7f211c50063e7192b73
Gabriel Milanez <159274083+gmilanezz@users.noreply.github.com>
2025-07-06 12:27:55 -0300

    upload de pasta funções

---
+++ funcoes/src/funcoes.java
@@ -0,0 +1,12 @@
+public class funcoes {
+    public static void main(String[] args) {
+        sum(15, 15);
+    }
+
+    static void sum(int x, int y) {
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x / y);
+        System.out.println(x * y);
+    }
+}
