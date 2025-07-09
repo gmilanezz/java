dc56c5eaa32eb7d60ffb2635304b7da948c77d85
Gabriel Milanez <159274083+gmilanezz@users.noreply.github.com>
2025-07-09 20:05:38 -0300

    Criacao de Pasta - Tipos Nao Primitivos

---
+++ TiposNaoPrimitivos/src/TiposNaoPrimitivos.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class TiposNaoPrimitivos {
+
+    public static void main(String[] args){
+        int x = 2;
+        Integer y = 4;
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
