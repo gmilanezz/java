75c99f82d124ce05d4a551d2b20a1246cf59fc1c
Gabriel Milanez <159274083+gmilanezz@users.noreply.github.com>
2025-07-31 20:14:44 -0300

    Criando Pasta - ToString

---
+++ ToString/src/co/gabrielmilanez/ToString.java
@@ -0,0 +1,10 @@
+package co.gabrielmilanez;
+
+public class ToString {
+
+    public static void main(String[] args){
+        User userA = new User ("Gabriel", "Milanez");
+
+        System.out.println(userA.toString());
+    }
+}
